d89c48f3709997eb7ee49e13274792a045f02114
Александр Щербаков <alex17.05.84@yandex.ru>
2023-06-27 15:01:51 +0300

    Произведение двух чисел

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int num = a*b;
+Console.WriteLine("Произведение двух чисел равно " + num); 
+
+// ﻿int numberA = new Random().Next(1, 10); // 1 2 3 4...9
+// Console.WriteLine(numberA);
+// int numderB = new Random().Next(1, 10);
+// Console.WriteLine(numderB);
+// int result = numberA + numderB;
+// Console.WriteLine(result);
